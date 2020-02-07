ca94afeaa49348775e220f00f4e732a879a6169f
Benjamin Most <benjaminmost@gmail.com>
2020-02-07 15:24:49 -0500

    Added conf file to start game in fullscreen

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+	t.title = "Plant Puzzle Game"
+	t.window.width=1600
+	t.window.height=900
+	t.window.fullscreen = true
+	t.console=false
+end
--- main.lua
+++ main.lua
@@ -1,13 +1,14 @@
 love.graphics.setDefaultFilter("nearest")
 
 function love.load()
-	print("loading game")
+	player = {
+		sprite = love.graphics.newImage("graphics/player.png")
+	}
 end
  
 function love.update(dt)
-	print("updating game")
 end
 
 function love.draw()
-	print("drawing images")
+	love.graphics.draw(player.sprite, 0, 0, 0, 1, 1, 0, 32)
 end
