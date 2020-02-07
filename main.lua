fbec6fdefcae8eb08b121becacd98ef770520bf5
Benjamin Most <benjaminmost@gmail.com>
2020-02-07 15:27:09 -0500

    Added code to move player

--- main.lua
+++ main.lua
@@ -2,13 +2,29 @@ love.graphics.setDefaultFilter("nearest")
 
 function love.load()
 	player = {
-		sprite = love.graphics.newImage("graphics/player.png")
+		x = 0,
+		y = 0,
+		sprite = love.graphics.newImage("graphics/player.png"),
+		speed = 5
 	}
 end
  
 function love.update(dt)
+	if love.keyboard.isDown("up") then
+        player.y = player.y - player.speed
+    end 
+    if love.keyboard.isDown("down") then
+        player.y = player.y + player.speed
+    end
+	if love.keyboard.isDown("left") then
+        player.x = player.x - player.speed
+    end
+    if love.keyboard.isDown("right") then
+        player.x = player.x + player.speed
+    end
+
 end
 
 function love.draw()
-	love.graphics.draw(player.sprite, 0, 0, 0, 1, 1, 0, 32)
+	love.graphics.draw(player.sprite, player.x, player.y, 0, 1, 1, 0, 32)
 end
