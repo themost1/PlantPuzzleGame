fccda68d731c9106a0e16922a4cbc75c5de5a61b
Benjamin Most <benjaminmost@gmail.com>
2020-02-15 15:56:04 -0500

    Added sample mouse code from meeting

--- main.lua
+++ main.lua
@@ -8,6 +8,15 @@ function love.load()
 		speed = 5
 	}
 
+	tilesPressed = { }
+	for i = 1, 9 do
+		local row = {}
+		for j = 1, 16 do
+			row[#row+1] = 0
+		end
+		tilesPressed[#tilesPressed+1] = row
+	end
+
 	dirtImage = love.graphics.newImage("graphics/dirt.jpg")
 end
  
@@ -33,9 +42,26 @@ function love.draw()
 	local dirtWidth = dirtImage:getWidth() * dirtScale
 	for row = 1, 9 do
 		for col = 1, 16 do
-			love.graphics.draw(dirtImage, dirtWidth * col, dirtHeight * row, 0, 1, 1, 0, 32)
+			if tilesPressed[row][col] == 0 then
+				love.graphics.draw(dirtImage, dirtWidth * col, dirtHeight * row, 0,
+						dirtWidth/dirtImage:getWidth(), dirtHeight/dirtImage:getHeight(), 32)
+			else
+				--love.graphics.draw(dirtImage, dirtWidth * col, dirtHeight * row, 0, 1, 1, 0, 32)
+			end
 		end
 	end
 
 	love.graphics.draw(player.sprite, player.x, player.y, 0, 1, 1, 0, 32)
+end
+
+function love.mousepressed(x, y, button, istouch)
+	local tileY = 0
+	local tileX = 0
+	local dirtScale = 1/4
+	local dirtHeight = dirtImage:getHeight() * dirtScale
+	local dirtWidth = dirtImage:getWidth() * dirtScale
+
+	tileY = math.floor(y / dirtHeight)
+	tileX = math.floor(x / dirtWidth)
+	tilesPressed[tileY][tileX] = 1
 end
