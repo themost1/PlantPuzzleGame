5f7b32ababd91eb38f90f273ebdaeffb33a58439
Benjamin Most <benjaminmost@gmail.com>
2020-02-29 17:20:41 -0500

    Fixed mouse offset issues!

--- main.lua
+++ main.lua
@@ -13,8 +13,8 @@ function love.load()
 		aTileMatrix[i] = {}
 		for j = 1,16 do
 			plantObject = plant:new()
-			if i % 2 == 0 or i % 2 == 1 then
-				plantObject = plant:new()
+			if i % 2 == j % 2 then
+				plantObject = bamboo:new()
 			end
 			plantObject:onLoad()
 			aTileMatrix[i][j] = plantObject
@@ -59,12 +59,13 @@ function love.draw()
 			plantImage = plantObject:getImage()
 			plantXScale = plantSize / plantImage:getWidth()
 			plantYScale = plantSize / plantImage:getHeight()
-			love.graphics.draw(plantImage, plantSize * (col-1) + plantStartX, plantSize * (row-1) + plantStartY, 0,
-					plantXScale, plantYScale, 32)
+			local startX = plantSize * (col-1) + plantStartX
+			local startY = plantSize * (row-1) + plantStartY
+			love.graphics.draw(plantImage, startX, startY, 0,
+					plantXScale, plantYScale, 0)
 		end
 	end
-
-	love.graphics.draw(player.sprite, player.x, player.y, 0, 1, 1, 0, 32)
+	love.graphics.draw(player.sprite, player.x, player.y, 0, 1, 1, 0, 0)
 end
 
 function love.mousepressed(x, y, button, istouch)
@@ -73,9 +74,11 @@ function love.mousepressed(x, y, button, istouch)
 
 	local plantX = x - plantStartX
 	local plantY = y - plantStartY
+	print("pressed: "..x.." "..y)
 
 	tileY = math.ceil( plantY / plantSize )
 	tileX = math.ceil( plantX / plantSize )
+	print(tileX .. " " .. tileY)
 
 	if tileY >= 1 and tileY <= 9 and tileX >= 1 and tileX <= 16 then
 		aTileMatrix[tileY][tileX]:onClick()
