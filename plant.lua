335830a51a674b580bfee5b432e0c13a24bc3403
Benjamin Most <benjaminmost@gmail.com>
2020-03-07 16:40:34 -0500

    Added id field to plants; added setup for json room storage (not implemented yet)

---
+++ plant.lua
@@ -0,0 +1,38 @@
+require('scripts.object')
+
+plant = object:new{
+	name = "Base Plant",
+	id = "",
+	image = nil,
+	watered = false,
+	passable = true
+}
+
+function plant:onLoad()
+	self.image = love.graphics.newImage("graphics/grass.png")
+end
+
+function plant:onClick()
+	if(selected == "water") then
+		self:onWater()
+	end
+end
+
+function plant:getImage()
+	return self.image
+end
+
+function plant:onWater()
+end
+
+
+bamboo = plant:new {
+	name = "Bamboo",
+	image = nil,
+	passable = false,
+	id = "bamboo"
+}
+
+function bamboo:onLoad()
+	self.image = love.graphics.newImage("graphics/bamboo_tile.png")
+end
